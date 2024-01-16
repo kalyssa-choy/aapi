0789d6874b9883e761316d5e4d0ea8054a2b62f7
StudentAM <studentam@Administrators-MacBook-Pro.local>
2024-01-16 08:22:12 -0800

    adding js

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 const submit = document.getElementById("addStory");
+const allStories = document.getElementById("stories");
+const userTitle = document.getElementById("userStoryTitle");
+const userStory = document.getElementById("userStory");
 
-function add(){
-    
+console.log("hello");
+
+function addStory(){
+    var newStory = document.createElement("div");    
+    var title = document.createElement("h2");
+    console.log(userTitle.value);
+    title.innerText = userTitle.value;
+    var theStory = document.createElement("p");
+    theStory.innerText = userStory.value;
+
+    newStory.appendChild(title);
+    newStory.appendChild(theStory);
+
+    allStories.appendChild(newStory);
 }
+
+submit.addEventListener("click", addStory);
